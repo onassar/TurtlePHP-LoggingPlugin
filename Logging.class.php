5c96d1446135e4824a24f2ea4c017d471135d677
Oliver Nassar <onassar@gmail.com>
2012-01-08 17:15:16 -0500

    Doc updates

--- Logging.class.php
+++ Logging.class.php
@@ -6,6 +6,17 @@
     /**
      * Logging
      * 
+     * TurtlePHP Logging Plugin which allows for the modification of PHP's
+     * default error logging.
+     * 
+     * This plugin does not modify how to issue an error log directive; rather
+     * it allows for the following customization:
+     *  - error log path (defaults to <.>)
+     *  - error reporting sensitivity (defaults to <E_ALL | E_STRICT>)
+     *  - error log filesize limitation (defaults to 0)
+     *  - whether or not errors should be logged (defaults to <true>)
+     *  - whether or not errors should be displayed (defaults to <false>)
+     * 
      * @author   Oliver Nassar <onassar@gmail.com>
      * @abstract
      */
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 TurtlePHP Logging Plugin
 ===
 
+TurtlePHP Logging Plugin which allows for the modification of PHP's
+default error logging.
+
+This plugin does not modify how to issue an error log directive; rather
+it allows for the following customization:
+ - error log path (defaults to <.>)
+ - error reporting sensitivity (defaults to <E_ALL | E_STRICT>)
+ - error log filesize limitation (defaults to 0)
+ - whether or not errors should be logged (defaults to <true>)
+ - whether or not errors should be displayed (defaults to <false>)
+
+
+### Example Initialization and Startup
     /**
      * Logging
      */
